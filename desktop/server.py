4e2738aa1650f66bd043a3c9c6adef1800e97010
Ognjen Galić <smclt30p@gmail.com>
2017-04-08 21:40:15 +0200

    Wrap in class

--- desktop/server.py
+++ desktop/server.py
@@ -1,53 +1,74 @@
+import traceback
+
 import serial
 
 
-def checksum(data):
-    check = 0
-    for i in range(0, 31):
-        if (data[i]) % 2 == 0:
-            check += 1
-    return check
+class ArdADC():
 
-def make_packet(string):
-    data = []
-    for i in string:
-        data.append(ord(i))
-    for i in range(0, 31 - len(data)):
-        data.append(0)
-    data.append(checksum(data))
-    return data
+    def __init__(self):
 
-def get_data(incoming):
-    data = []
-    for i in incoming:
-        if i == 0: # null terminated
-            string = ""
-            for i in data:
-                string += i
-            return string
-        data.append(chr(i))
+        try:
+            self.serial = serial.Serial("/dev/ttyUSB0", timeout=5, baudrate=9600)
+        except BaseException as e:
+            raise ArdADC.ArdADCException(e)
+
+    def handshake(self):
+
+        syn = self.serial.read(32)
+
+        if self.checksum(syn) != syn[-1]:
+            raise ArdADC.ArdADCException("SYN Checksum invalid")
+
+        if self.get_data(syn) != "SYN":
+            ArdADC.ArdADCException("SYN invalid")
+
+        self.serial.write(self.make_packet("ACK"))
+        self.serial.write(self.make_packet("SYN"))
+
+        ack = self.serial.read(32)
+
+        if self.checksum(ack) != ack[-1]:
+            ArdADC.ArdADCException("ACK checksum invalid!")
+
+        if self.get_data(ack) != "ACK":
+            ArdADC.ArdADCException("ACK invalid")
+
+    def checksum(self, data):
+        check = 0
+        for i in range(0, 31):
+            if (data[i]) % 2 == 0:
+                check += 1
+        return check
+
+    def make_packet(self, string):
+        data = []
+        for i in string:
+            data.append(ord(i))
+        for i in range(0, 31 - len(data)):
+            data.append(0)
+        data.append(self.checksum(data))
+        return data
+
+    def get_data(self, incoming):
+        data = []
+        for i in incoming:
+            if i == 0:  # null terminated
+                string = ""
+                for i in data:
+                    string += i
+                return string
+            data.append(chr(i))
+
+    class ArdADCException(BaseException):
+        pass
 
 if __name__ == "__main__":
 
-    ser = serial.Serial("/dev/ttyUSB0", timeout=5)
+    try:
 
-    syn = ser.read(32)
+        adc = ArdADC()
+        adc.handshake()
 
-    if checksum(syn) != syn[-1]:
-        print("Checksum invalid")
-
-    if get_data(syn) != "SYN":
-        print("SYN invalid")
-
-    ser.write(make_packet("ACK"))
-    ser.write(make_packet("SYN"))
-
-    ack = ser.read(32)
-
-    if checksum(ack) != ack[-1]:
-        print("ACK checksum invalid!")
-
-    if get_data(ack) != "ACK":
-        print("ACK invalid")
-
+    except ArdADC.ArdADCException:
+        traceback.print_exc()
 
